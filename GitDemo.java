d0026715d231df00639bddf860541dd3b3b591a0
shipeng <987336340@qq.com>
2019-06-21 00:34:27 +0800

    修改代码

--- GitDemo.java
+++ GitDemo.java
@@ -1,5 +1,8 @@
 package cn.itcast.git;
 
 public class GitDemo {
-
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+        System.out.println("提交测试~~~~");
+    }
 }
